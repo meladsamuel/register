83933e3bad745cf5757a80feb2c3916025ec4b18
meladsamuel <meladsamuel2@gmail.com>
2020-09-03 13:26:22 +0200

    make it better

--- app/controllers/NotFoundController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace shfretak\controllers;
-class NotFoundController extends AbstractController{
-      public function NotFoundAction(){
-            $this->language->load('notFound\default');
-            $this->language->load('template\common');
-            $this->_view();
-      }
-}
--- app/lib/IsAuth.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace app\lib;
-class IsAuth {
-      public function isAuth($userAcouont ){
-
-      }
-}
