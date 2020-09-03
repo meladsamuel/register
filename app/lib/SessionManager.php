6908e0dc670ba793e5e8ea40892bd6a91a7fb490
meladsamuel <meladsamuel2@gmail.com>
2020-09-03 14:21:31 +0200

    make it better

--- app/lib/SessionManager.php
+++ app/lib/SessionManager.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace shfretak\lib;
+namespace app;
 
 class SessionManager extends \SessionHandler
 {
--- public/index.php
+++ public/index.php
@@ -1,10 +1,12 @@
 <?php
 
+use app\SessionManager;
+
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 require '..' . DS . 'app' . DS . 'config.php';
 require '..' . DS . 'vendor' . DS . 'autoload.php';
 echo \app\test::print();
-$session = new \shfretak\lib\SessionManager();
+$session = new SessionManager();
 $session->start();
 $session->new = 'fsdjkfl;asd';
 $session->dump();
