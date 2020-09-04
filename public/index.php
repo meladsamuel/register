b565a2c63b6dfe60b085b63e512ef73ff8cbded3
meladsamuel <meladsamuel2@gmail.com>
2020-09-04 08:10:00 +0200

    test 2

--- public/index.php
+++ public/index.php
@@ -15,14 +15,18 @@ $password = '7RAprBQqD:W_"b^ce}s!';
 $dbname = "loginSys";
 
 //$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+if(UsersModel::userExisting('ali')){
+    echo 'user exit ';
+}else {
+    $user = new UsersModel();
+    $user->username = 'ali';
+    $user->email = 'ali@ali.eg';
+    $user->password = 'ali123';
+    $user->reg_date = date('Y-m-d h:i:s');
+    $user->group_id = 1;
+    $user->save();
 
+}
 
-$user = new UsersModel();
-$user->username = 'ali';
-$user->email = 'ali@ali.eg';
-$user->password = 'ali123';
-$user->reg_date = date('Y-m-d h:i:s');
-$user->group_id = 1;
-$user->save();
 
 var_dump(UsersModel::getAll());
